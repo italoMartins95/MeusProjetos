927d83fc26920b789803a2a862ddb48d8136f7c0
Italo Martins <italomartinssk88@gmail.com>
2022-06-16 15:42:19 -0300

    Update calculadora.js

--- projects/calculadora/scripts/calculadora.js
+++ projects/calculadora/scripts/calculadora.js
@@ -1,19 +1,44 @@
-var display = window.document.getElementById('display')
-var resultado
+var displayContent = window.document.getElementById('displayContent')
+var displayCalc = window.document.getElementById('displayCalc')
 
-function botoes(num){
-    
-    display.textContent += num
+function botoes(num){    
+    displayCalc.textContent += num
 }
 
 function clean(){
-    display.textContent = ""
+    displayCalc.textContent = ""
+    displayContent.innerText = ""
+
+    displayContent.style.borderColor = "white"
 }
 
 function calcular(){
-    resultado = eval(display.textContent)
+    try{
+    	var resultado = eval(displayCalc.textContent)
+    	var p = document.createElement('p');
 
-    display.textContent = resultado
+    	p.innerText = `${displayCalc.textContent} = ${resultado.toFixed(2)}`
+    
+    	if(displayContent.children.length >= 3){
+		displayContent.removeChild(displayContent.childNodes[0])
+		displayContent.appendChild(p)
+    	}else{
+		displayContent.appendChild(p)
+    	}
+    }catch (error){
+	displayCalc.textContent = ""
+	console.log(error)
+	window.alert("Algo deu errado, tente novamente!")
+    }
 
+    displayCalc.textContent = ""
+    bordContent()
+}
 
+function bordContent(){
+    if(displayContent.innerText == null){
+	displayContent.style.borderColor = "white"
+    }else{
+	displayContent.style.borderColor = "black"
+    }
 }
